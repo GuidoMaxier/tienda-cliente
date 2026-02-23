02c62edf07398773871367dbab35916eb8537abe
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2026-02-22 23:22:10 -0300

    feat: Initialize Stripe client and remove `PRICE` example from environment variables.

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-15.clover",
+  apiVersion: "2026-01-28.clover",
   appInfo: {
     name: "stripe-samples/checkout-one-time-payments",
     version: "0.0.1",
